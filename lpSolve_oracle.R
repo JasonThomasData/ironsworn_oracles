52b6006ce628aa85e8720d90eba87f44f59b5b41
john smith <john@john.john>
2021-02-12 14:12:15 +1100

    More args, inc minimum probability for foes to appear

--- lpSolve_oracle.R
+++ lpSolve_oracle.R
@@ -1,28 +1,15 @@
 #!/usr/bin/env Rscript
 
 # sudo apt install r-base
-#install.packages("argparser")
-#install.packages("lpSolveAPI")
 
-library("argparser")
-library("lpSolveAPI")
+packages = c("argparser", "lpSolveAPI")
 
-# From randomEnemyEncounter.odt
-# Foe | Type | Rank
-# --- | --- | ---
-# Common folk | Ironlander | Troublesome
-# Marsh rat   | Animal | Troublesome
-# Raider | Ironlander | Dangerous
-# Mystic | Ironlander | Dangerous
-# Warrior | Ironlander | Dangerous
-# Bonewalker | Horror | Dangerous
-# Sodden | Horror | Formidable most common horror
-# Haunt | Horror | Formidable
-# Tawny Wyvern | Beast | Extreme
-# Sea Primordial | Firstborn | Extreme
-# Iron Revenant | Horror | Extreme
-# Chimera | Horror | Extreme
-# Leviathan | Beast | Epic
+for(package in packages) {
+    if (!require(package, character.only = TRUE)) {
+      install.packages(package, dependencies = TRUE)
+    }
+    library(package, character.only = TRUE)
+}
 
 getCoefficientsForValueInColumn = function(foeData, nominatedValue, columnName) {
     coefficients = c() 
@@ -45,22 +32,40 @@ parser = arg_parser("Generate enemy encounter oracles for Ironsworn by region")
 parser = add_argument(parser, "--region", help="The region to generate the encounter table for", type="character")
 parser = add_argument(parser, "--input", help="The file for foes found within the region", type="character")
 parser = add_argument(parser, "--output", help="The same file, with dice rolls prepended", type="character")
+parser = add_argument(parser, "--minProb", help="The minimum probability that a creature will appear in the output oracle, if it's present in the csv of enemies. Valid between 0,1", default=0.02)
+parser = add_argument(parser, "--rollScale", help="The absolute maximum valid roll, which should be above 100, so that a character with no experience should never meet Epic foes.", default=120)
 argv = parse_args(parser)
 regions = c("Barrier Islands", "Ragged Coast", "Deep Wilds", "Flooded Lands", "Havens", "Hinterlands", "Tempest Hills", "Veiled Mountains", "Shattered Wastes")
 if(!is.element(argv$region, regions)) {
     print(parser)
+    return (1)
 }
 region = argv$region
+if(argv$minProb < 0 || argv$minProb > 1) {
+    print(parser)
+    return (1)
+}
+minProb = argv$minProb
+if(argv$rollScale < 100) {
+    print(parser)
+    return (1)
+}
 foeData = read.csv(argv$input)
 outputFileName = argv$output
 
-
+writeLines("Using foe data for this region:")
+print(foeData)
 
 foeTypeProbababilities = read.csv("foe_type_probabilities.csv")
 foeRankProbababilities = read.csv("foe_rank_probabilities.csv")
 
 foeTypeProbababilitiesForRegion = getProbabilitiesForThisRegion(foeTypeProbababilities, region)
 foeRankProbababilitiesForRegion = getProbabilitiesForThisRegion(foeRankProbababilities, region)
+
+writeLines("Using foe types:")
+print(foeTypeProbababilitiesForRegion)
+writeLines("Using foe ranks:")
+print(foeRankProbababilitiesForRegion)
 
 numberOfFoes = length(foeData[,"Foe"])
 objective_coefficients = rep(1, numberOfFoes) #One for each foe
@@ -90,24 +95,16 @@ add.constraint(lp_model, formidableCoefficients, "<=", foeRankProbababilitiesFor
 add.constraint(lp_model, extremeCoefficients, "<=", foeRankProbababilitiesForRegion[, "Extreme"])
 add.constraint(lp_model, epicCoefficients, "<=", foeRankProbababilitiesForRegion[, "Epic"])
 
-# All creatures have a minimum 2% chance
-add.constraint(lp_model, c(1,0,0,0,0,0,0,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,1,0,0,0,0,0,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,1,0,0,0,0,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,1,0,0,0,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,1,0,0,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,1,0,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,1,0,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,0,1,0,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,0,0,1,0,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,0,0,0,1,0,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,0,0,0,0,1,0,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,0,0,0,0,0,1,0), ">", 0.02)
-add.constraint(lp_model, c(0,0,0,0,0,0,0,0,0,0,0,0,1), ">", 0.02)
+for(i in 1:numberOfFoes) {
+    constraint_dummy = rep(0, numberOfFoes)
+    constraint_dummy[i] = 1
+    add.constraint(lp_model, constraint_dummy, ">", minProb)
+}
 
 solve(lp_model)
 
 probabilities=get.variables(lp_model)
+writeLines("probabilities based on ordered list of foes")
 print(probabilities)
 
 tableRange=120
@@ -115,8 +112,6 @@ minimumRoll=1
 rolls = c()
 newTableWithRolls = data.frame()
 
-print("...")
-print("probabilities based on ordered list of foes")
 for(probability in probabilities) {
     roll=probability*tableRange
     roundedRoll=round(roll, digits=0)
@@ -129,3 +124,7 @@ foeData$Rolls <- rolls
 
 foeData = foeData[,c(4,1,2,3)]
 write.csv(foeData, outputFileName, row.names=F)
+
+writeLines("Result:")
+print(foeData)
+writeLines(sprintf("File saved at %s", outputFileName))
