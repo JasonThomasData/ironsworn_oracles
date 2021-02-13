591469039df5ab5f85a103fc8816f36cb5d502c5
john smith <john@john.john>
2021-02-13 18:56:56 +1100

    Fixing incorrect table, incorrect link

--- encounter_tables/generate_tables.sh
+++ encounter_tables/generate_tables.sh
@@ -2,7 +2,7 @@
 
 ./lpSolve_oracle.R --input foes/barrier_islands_foes.csv --region "Barrier Islands" --output oracles/barrier_islands_oracle.csv --rollScale 115
 ./lpSolve_oracle.R --input foes/ragged_coast_foes.csv --region "Ragged Coast" --output oracles/ragged_coast_oracle.csv --rollScale 118
-./lpSolve_oracle.R --input foes/deep_wilds_foes.csv --region "Deep Wilds" --output oracles/deep_wilds_oracle.csv --rollScale 130
+./lpSolve_oracle.R --input foes/deep_wilds_foes.csv --region "Deep Wilds" --output oracles/deep_wilds_oracle.csv --rollScale 105
 ./lpSolve_oracle.R --input foes/flooded_lands_foes.csv --region "Flooded Lands" --output oracles/flooded_lands_oracle.csv 
 ./lpSolve_oracle.R --input foes/havens_foes.csv --region "Havens" --output oracles/havens_oracle.csv --minProb 0.01 --rollScale 105
 ./lpSolve_oracle.R --input foes/hinterlands_foes.csv --region "Hinterlands" --output oracles/hinterlands_oracle.csv --rollScale 105
